4e4f655348bff665bbdd52cf0bc3afeaf2637ed9
imre-gabor <gabor@aut.bme.hu>
2021-10-13 19:16:23 +0200

    solution at live 1

--- src/main/java/hu/webuni/hr/minta/service/SmartEmployeeService.java
+++ src/main/java/hu/webuni/hr/minta/service/SmartEmployeeService.java
@@ -25,16 +25,41 @@ public class SmartEmployeeService implements EmployeeService {
 		double yearsWorked = ChronoUnit.DAYS.between(employee.getDateOfStartWork(), LocalDateTime.now()) / 365.0;
 		
 		Smart smartConfig = config.getSalary().getSmart();
-		if(yearsWorked > smartConfig.getLimit3())
-			return smartConfig.getPercent3();
+		//1. megoldás
+//		if(yearsWorked > smartConfig.getLimit3())
+//			return smartConfig.getPercent3();
+//		
+//		if(yearsWorked > smartConfig.getLimit2())
+//			return smartConfig.getPercent2();
+//		
+//		if(yearsWorked > smartConfig.getLimit1())
+//			return smartConfig.getPercent1();
 		
-		if(yearsWorked > smartConfig.getLimit2())
-			return smartConfig.getPercent2();
+		TreeMap<Double, Integer> limits = smartConfig.getLimits();
 		
-		if(yearsWorked > smartConfig.getLimit1())
-			return smartConfig.getPercent1();
+		//2. megoldás
+//		Integer maxLimit = null;
+//		for(Entry<Double, Integer> entry: limits.entrySet()) {
+//			if(yearsWorked > entry.getKey())
+//				maxLimit = entry.getValue();
+//			else
+//				break;
+//		}
+//		
+//		return maxLimit == null ? 0 : maxLimit;
+
+		//3. megoldás
+//		Optional<Double> optionalMax = limits.keySet().stream()
+//				.filter(k -> yearsWorked >= k)
+//				.max(Double::compare);
+//		
+//		return optionalMax.isEmpty() ? 0 : limits.get(optionalMax.get());
+
 		
-		return 0;
+		//4. megoldás
+		Entry<Double, Integer> floorEntry = limits.floorEntry(yearsWorked);
+		return floorEntry == null ? 0 : floorEntry.getValue();
+		
 	}
 
 }
