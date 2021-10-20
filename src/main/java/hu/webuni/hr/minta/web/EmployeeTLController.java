6ef1cc6ccf5b8767759ccf4f5d4f586b41ce9540
imre-gabor <imre.gabor@autsoft.hu>
2021-10-20 18:15:42 +0200

    controller methods for thymeleaf task

--- src/main/java/hu/webuni/hr/minta/web/EmployeeTLController.java
+++ src/main/java/hu/webuni/hr/minta/web/EmployeeTLController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import hu.webuni.hr.minta.model.Employee;
@@ -32,5 +33,35 @@ public class EmployeeTLController {
 		allEmployees.add(employee);
 		return "redirect:employees";
 	}
+	
+	
+	@GetMapping("/deleteEmployee/{id}")
+	public String deleteEmployee(@PathVariable long id) {
+		allEmployees.removeIf(emp -> emp.getEmployeeId().equals(id));
+		return "redirect:/employees";
+	}
+	
+	
+	@GetMapping("/employees/{id}")
+	public String editEmployee(@PathVariable long id, Map<String, Object> model) {
+		model.put("employee", allEmployees.stream().filter(emp -> emp.getEmployeeId().equals(id)).findFirst().get());
+		
+		return "editEmployee";
+	}
+	
+	@PostMapping("/updateEmployee")
+	public String updateEmpployee(Employee employee) {
+		
+		for(int i=0; i<allEmployees.size(); i++) {
+			if(allEmployees.get(i).getEmployeeId().equals(employee.getEmployeeId())) {
+				allEmployees.set(i, employee);
+				break;
+			}
+		}
+		
+		return "redirect:employees";
+	}
+	
+	
 
 }
